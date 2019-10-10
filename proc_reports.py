4114de18df81575d4aa3598d6b3128b7ecb40ef8
Dan Mick <dan.mick@redhat.com>
2019-10-09 17:36:20 -0700

    proc_reports.py: cleanup, process only new reports
    Signed-off-by: Dan Mick <dan.mick@redhat.com>

--- proc_reports.py
+++ proc_reports.py
@@ -1,95 +1,150 @@
+#!/usr/bin/env python3
+# vim: ts=4 sw=4 expandtab:
+import hashlib
 import json
 import psycopg2
+import sys
 
-f = open('/opt/telemetry/pg_pass.txt', 'r')
-password = f.read().strip()
-f.close()
+conn = None
 
-conn = psycopg2.connect(
-    host='localhost',
-    database='telemetry',
-    user='telemetry',
-    password=password
-)
-rcur = conn.cursor()
-rcur.execute("SELECT cluster_id, report FROM report")
-for row in rcur.fetchall():
-    cluster_id = row[0]
-    report = json.loads(row[1])
-    ts = report.get('report_timestamp')
 
-    ccur = conn.cursor()
+def sanitize_backtrace(bt):
+    ret = list()
+    for func_record in bt:
+        # split into two fields on last space, take the first one,
+        # strip off leading ( and trailing )
+        func_plus_offset = func_record.rsplit(' ', 1)[0][1:-1]
+        ret.append(func_plus_offset.split('+')[0])
 
-    # cluster
-    ccur.execute("SELECT cluster_id,latest_report_stamp FROM cluster WHERE cluster_id=%s", (cluster_id,))
-    crows = ccur.fetchall()
-    update = True
-    if crows:
-        crow = crows[0]
-        if str(crow[1]) > ts:
-            print('cluster %s already has newer report %s' % (cluster_id, crow[1]))
-            update = False
+    return ret
 
-    if update:
-        print('updating %s' % (cluster_id))
-        num_pgs = 0
-        for pool in report.get('pools', []):
-            num_pgs += pool.get('pg_num', 0)
-        ccur.execute(
-            "INSERT INTO cluster (cluster_id, latest_report_stamp, num_mon, num_osd, num_pools, num_pgs, total_bytes, total_used_bytes) VALUES (%s, %s, %s, %s, %s, %s, %s, %s) ON CONFLICT (cluster_id) DO UPDATE SET latest_report_stamp=%s, num_mon=%s, num_osd=%s, num_pools=%s, num_pgs=%s, total_bytes=%s, total_used_bytes=%s",
-            (cluster_id,
-             ts,
-             report.get('mon', {}).get('count', 0),
-             report.get('osd', {}).get('count', 0),
-             report.get('usage', {}).get('pools', 0),
-             num_pgs,
-             report.get('usage', {}).get('total_bytes', 0),
-             report.get('usage', {}).get('total_used_bytes', 0),
-             ts,
-             report.get('mon', {}).get('count', 0),
-             report.get('osd', {}).get('count', 0),
-             report.get('usage', {}).get('pools', 0),
-             num_pgs,
-             report.get('usage', {}).get('total_bytes', 0),
-             report.get('usage', {}).get('total_used_bytes', 0)),)
 
-        # cluster_version
-        ccur.execute(
-            "DELETE FROM cluster_version WHERE cluster_id=%s",
-            (cluster_id,)
-        )
-        for (entity_type, info) in report.get('metadata', {}).items():
-            for (version, num) in info.get('ceph_version', {}).items():
-                ccur.execute(
-                    "INSERT INTO cluster_version (cluster_id, entity_type, version, num_daemons) VALUES (%s, %s, %s, %s)",
-                    (cluster_id,
-                     entity_type,
-                     version,
-                     num,))
+def calc_sig(funclist):
+    sig = hashlib.sha256()
+    for func in funclist:
+        sig.update(func.encode())
+    return ''.join('%02x' % c for c in sig.digest())
 
-    # crash
+
+def update_cluster(cluster_id, latest_report_ts, report):
+    cur = conn.cursor()
+    cur.execute("SELECT cluster_id,latest_report_stamp FROM cluster WHERE cluster_id=%s", (cluster_id,))
+    row = cur.fetchone()
+    if row:
+        latest_cluster_report_stamp = row[1]
+
+        if latest_cluster_report_stamp >= latest_report_ts:
+            return False
+
+    num_pgs = 0
+    for pool in report.get('pools', []):
+        num_pgs += pool.get('pg_num', 0)
+    cur.execute(
+        "INSERT INTO cluster (cluster_id, latest_report_stamp, num_mon, num_osd, num_pools, num_pgs, total_bytes, total_used_bytes) VALUES (%s, %s, %s, %s, %s, %s, %s, %s) ON CONFLICT (cluster_id) DO UPDATE SET latest_report_stamp=%s, num_mon=%s, num_osd=%s, num_pools=%s, num_pgs=%s, total_bytes=%s, total_used_bytes=%s",
+        (cluster_id,
+         latest_report_ts,
+         report.get('mon', {}).get('count', 0),
+         report.get('osd', {}).get('count', 0),
+         report.get('usage', {}).get('pools', 0),
+         num_pgs,
+         report.get('usage', {}).get('total_bytes', 0),
+         report.get('usage', {}).get('total_used_bytes', 0),
+
+         latest_report_ts,
+         report.get('mon', {}).get('count', 0),
+         report.get('osd', {}).get('count', 0),
+         report.get('usage', {}).get('pools', 0),
+         num_pgs,
+         report.get('usage', {}).get('total_bytes', 0),
+         report.get('usage', {}).get('total_used_bytes', 0)),)
+    return True
+
+
+def update_cluster_version(cluster_id, latest_report_ts, report):
+    cur = conn.cursor()
+    cur.execute(
+        "DELETE FROM cluster_version WHERE cluster_id=%s",
+        (cluster_id,)
+    )
+    for (entity_type, info) in report.get('metadata', {}).items():
+        for (version, num) in info.get('ceph_version', {}).items():
+            cur.execute(
+                "INSERT INTO cluster_version (cluster_id, entity_type, version, num_daemons) VALUES (%s, %s, %s, %s)",
+                (cluster_id,
+                 entity_type,
+                 version,
+                 num,))
+
+
+def update_crash(cluster_id, latest_report_ts, report):
+    cur = conn.cursor()
     crashes = report.get('crashes', [])
     if isinstance(crashes, dict):
         tmp = []
-        for c in crashes.valeus():
+        for c in crashes.values():
             tmp.append(c)
         crashes = tmp
 
+    update_count = 0
     for crash in crashes:
         crash_id = crash.get('crash_id')
         if not crash_id:
             continue
-        stack = str(crash.get('backtrace'))
-        ccur.execute(
-            "INSERT INTO crash (crash_id, cluster_id, raw_report, timestamp, entity_name, version, stack) values (%s, %s, %s, %s, %s, %s, %s) ON CONFLICT DO NOTHING",
+        stack = crash.get('backtrace')
+        funclist = sanitize_backtrace(stack)
+        sig = calc_sig(funclist)
+        cur.execute(
+            "INSERT INTO crash (crash_id, cluster_id, raw_report, timestamp, entity_name, version, stack_sig, stack) values (%s, %s, %s, %s, %s, %s, %s, %s) ON CONFLICT DO NOTHING",
             (crash_id,
              cluster_id,
              json.dumps(crash, indent=4),
              crash.get('timestamp'),
              crash.get('entity_name'),
              crash.get('ceph_version'),
-             stack,
+             sig,
+             str(stack),
             ))
+        update_count += int(cur.statusmessage.split()[2])
+    return len(crashes), update_count
 
+
+def main():
+    f = open('/opt/telemetry/pg_pass.txt', 'r')
+    password = f.read().strip()
+    f.close()
+
+    global conn
+    conn = psycopg2.connect(
+        host='localhost',
+        database='telemetry',
+        user='telemetry',
+        password=password
+    )
+
+    ccur = conn.cursor()
+    rcur = conn.cursor()
+
+    cluster_count = update_count = crash_count = crash_update_count = 0
+    ccur.execute("SELECT DISTINCT cluster_id from report")
+    for cid in ccur.fetchall():
+        cid = cid[0]
+        rcur.execute("SELECT report_stamp, report FROM report WHERE cluster_id=%s ORDER BY report_stamp DESC LIMIT 1", (cid,))
+        latest_report_ts, report = rcur.fetchone()
+        try:
+            report = json.loads(report)
+        except TypeError:
+            print('cluster %s ts %s has malformed report' % (cid, latest_report_ts))
+            continue
+        cluster_count += 1
+        if update_cluster(cid, latest_report_ts, report):
+            update_count += 1
+        update_cluster_version(cid, latest_report_ts, report)
+        visited, updated = update_crash(cid, latest_report_ts, report)
+        crash_count += visited
+        crash_update_count += updated
+    print('updated %d/%d clusters, updated %d/%d crashes' % (update_count, cluster_count, crash_update_count, crash_count))
     conn.commit()
 
+
+if __name__ == '__main__':
+    sys.exit(main())
