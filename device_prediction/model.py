313bc34c4e321a2a6c60dbba1fbb96e7e79b4448
Yaarit Hatuka <yaarit@redhat.com>
2020-08-19 15:30:05 -0400

    add device capacity to JSON samples
    Signed-off-by: Yaarit Hatuka <yaarit@redhat.com>

--- device_prediction/model.py
+++ device_prediction/model.py
@@ -15,22 +15,22 @@ class PreditionResult(Enum):
     GOOD = 0
     FAIL = 1
 
-def simple_prediction(smart_data):
-    if len(smart_data) == 0:
+def simple_prediction(device_data):
+    if len(device_data) == 0:
         return "Invalid prediction input"
     # Get most recent report
-    report_date = sorted(smart_data.keys(), reverse=True)[0]
-    report = smart_data[report_date]["attr"]
-    if s_Reallocated_Sector_Count in report and report[s_Reallocated_Sector_Count]["val_raw"] > 0:
+    report_date = sorted(device_data['smart_data'].keys(), reverse=True)[0]
+    report = device_data['smart_data'][report_date]['attr']
+    if s_Reallocated_Sector_Count in report and report[s_Reallocated_Sector_Count]['val_raw'] > 0:
         return PreditionResult.FAIL
     return PreditionResult.GOOD
 
 def main():
     inp_json = sys.stdin.read()
-    smart_data = json.loads(inp_json)
-    prediction_result = simple_prediction(smart_data)
+    device_data = json.loads(inp_json)
+    prediction_result = simple_prediction(device_data)
 
-    print(prediction_result)
+    #print(prediction_result)
 
 if __name__ == '__main__':
     sys.exit(main())
--- device_prediction/predict_device.py
+++ device_prediction/predict_device.py
@@ -17,6 +17,20 @@ DSN = '/opt/telemetry/grafana.dsn'
 
 
 def get_prediction_input(conn, device_id):
+    prediction_input = {}
+
+    # Get device's capacity
+    cur = conn.cursor(cursor_factory=psycopg2.extras.DictCursor)
+    cur.execute("""SELECT device.spec.capacity
+                   FROM device.device
+                   INNER join device.spec on device.device.spec_id = device.spec.id
+                   WHERE device.device.id = %s
+                """, (device_id,))
+
+    capacity = cur.fetchone();
+    prediction_input['capacity_bytes'] = capacity[0] if capacity else None
+
+    # Get device's SMART attributes
     device_smart = {}
     rep_cur = conn.cursor(cursor_factory=psycopg2.extras.DictCursor)
     rep_cur.execute("""SELECT report_id
@@ -35,17 +49,19 @@ def get_prediction_input(conn, device_id):
                           """, (report_id,))
 
         for s in smart_cur:
-            date = s["ts"].__str__()
+            date = s['ts'].__str__()
             if date not in device_smart:
                 device_smart[date] = {}
-                device_smart[date]["attr"] = {}
+                device_smart[date]['attr'] = {}
 
-            device_smart[date]["attr"][s["attr_id"]] = {"name" : s["attr_name"], "val_raw" : s["attr_raw"], "val_norm": s["attr_norm"]}
+            device_smart[date]['attr'][s['attr_id']] = {'name' : s['attr_name'], 'val_raw' : s['attr_raw'], 'val_norm': s['attr_norm']}
         smart_cur.close()
 
+    prediction_input['smart_data'] = device_smart
+    cur.close()
     rep_cur.close()
 
-    return device_smart
+    return prediction_input
 
 def insert_result(conn, device_id, result):
     cur = conn.cursor(cursor_factory=psycopg2.extras.DictCursor)
@@ -69,10 +85,10 @@ def main():
     else:
         print(f"Usage: {sys.argv[0]} <device_id>", file=sys.stderr)
         sys.exit()
-    smart_data = get_prediction_input(conn, device_id)
-    smart_data_json = json.dumps(smart_data, indent=4, sort_keys=True)
-    #print(smart_data_json)
-    run_res = subprocess.run("./model.py", stdout=subprocess.PIPE, input=smart_data_json, encoding='ascii')
+    prediction_input = get_prediction_input(conn, device_id)
+    prediction_input_json = json.dumps(prediction_input, indent=4, sort_keys=True)
+    #print(prediction_input_json)
+    run_res = subprocess.run("./model.py", stdout=subprocess.PIPE, input=prediction_input_json, encoding='ascii')
     prediction_result = run_res.stdout
     print(f"Prediction result for {device_id} is: {prediction_result}")
     if prediction_result is not None:
